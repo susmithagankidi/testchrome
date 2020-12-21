8dd91c4284b1f62eb378259e35db98dcbe6c8975
susmithagankidi <gankidisusmitha@gmail.com>
2020-12-21 12:55:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import chromedriver_binary  # Adds chromedriver binary to path
 
-driver = webdriver.Chrome(executable_path=binary_path,chrome_options=chrome_options)
+options = Options()
+options.add_argument("--headless") #open Browser in maximized mode
+options.add_argument("--no-sandbox") #bypass OS security model
+options.add_argument("disable-extensions")
+options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
+driver = webdriver.Chrome(chrome_options=options)
+# executable_path=binary_path,
 driver.get("http://www.python.org")
 assert "Python" in driver.title
