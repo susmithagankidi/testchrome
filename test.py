ec9a2c5170d4a2033ebfc5d651f877742dc9b5e3
susmithagankidi <gankidisusmitha@gmail.com>
2020-12-21 13:04:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ options.add_argument("--headless") #open Browser in maximized mode
 options.add_argument("--no-sandbox") #bypass OS security model
 options.add_argument("disable-extensions")
 options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(executable_path=binary_path,chrome_options=chrome_options)
 # executable_path=binary_path,
 driver.get("http://www.python.org")
 assert "Python" in driver.title
