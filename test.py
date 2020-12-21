408579c416341f0693abb22511399490dbefd359
susmithagankidi <gankidisusmitha@gmail.com>
2020-12-21 12:41:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import chromedriver_binary  # Adds chromedriver binary to path
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path=binary_path,chrome_options=chrome_options)
 driver.get("http://www.python.org")
 assert "Python" in driver.title
